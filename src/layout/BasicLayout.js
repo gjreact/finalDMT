3896163331ef3a748019e739fad9d5c3529f5757
gjreact <gjreact@gmail.com>
2022-11-21 17:52:57 +0530

    sidemenu updated

--- src/layout/BasicLayout.js
+++ src/layout/BasicLayout.js
@@ -3,13 +3,14 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "../asset/css/slite.css";
 import SideMenu from "../pages/SideBar/SideMenu";
+import '../theme/theme.css';
 
 function BasicLayout() {
   return (
     <>
       <Container fluid className="border border-2 margin-0 containerr">
         <Row className="Roww">
-          <Col className="d-none d-md-block bg-dark col-2">
+          <Col className="d-none d-md-block sideMenuColor col-3">
             <div className="mt-3 d-flex justify-content-center">
              <img className="align-center" style={{width:"100%",maxWidth:"150px"}} src={logo} alt="Paymento_Logo" />
             </div>
--- src/pages/SideBar/SideMenu.js
+++ src/pages/SideBar/SideMenu.js
@@ -1,54 +1,60 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
+import "../../asset/css/slite.css";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function SideMenu() {
   return (
-    <div className="mt-5">
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
+    <div className="mt-5 bg-dark">
+      <Accordion className="" defaultActiveKey="0">
+        <Accordion.Item className="sideMenuColor" eventKey="0">
           <Accordion.Header>DMT</Accordion.Header>
-          <Accordion.Body>
-            
-          <Accordion defaultActiveKey="1">
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>Remitter</Accordion.Header>
-              <Accordion.Body>
-                <a href="https://www.google.com">ammacheee</a>
-              </Accordion.Body>
-            </Accordion.Item>
-            </Accordion>
+          <Accordion.Body className="zeropadding">
+          
+          <Accordion defaultActiveKey="0" className="sideMenuTransparent" flush>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Remitter</Accordion.Header>
+                <Accordion.Body className="sideMenuTransparent">
+                  <ListGroup style={{overflow:"hidden",margin:"0px"}}>
+                    <ListGroup.Item className="activeMenu"><a href="#">Remitter Information</a></ListGroup.Item>
+                    <ListGroup.Item><a href="#">Remitter Send-OTP</a></ListGroup.Item>
+                    <ListGroup.Item><a href="#">Remitter Resend- OTP</a></ListGroup.Item>
+                    <ListGroup.Item><a href="#">Verify Remitter OTP</a></ListGroup.Item>
+                    <ListGroup.Item><a href="#">Remitter Balance</a></ListGroup.Item>
+                  </ListGroup>
 
-            <Accordion defaultActiveKey="1">
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Remitter</Accordion.Header>
-              <Accordion.Body>
-                <a href="https://www.google.com">ammacheee</a>
-              </Accordion.Body>
-            </Accordion.Item>
-            </Accordion>
-
-            <Accordion defaultActiveKey="1">
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Remitter</Accordion.Header>
-              <Accordion.Body>
-                <a href="https://www.google.com">ammacheee</a>
-              </Accordion.Body>
-            </Accordion.Item>
-            </Accordion>
-
-            <Accordion defaultActiveKey="1">
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Remitter</Accordion.Header>
-              <Accordion.Body>
-                <a href="https://www.google.com">ammacheee</a>
-              </Accordion.Body>
-            </Accordion.Item>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item  className="overflow" eventKey="1">
+                <Accordion.Header>Beneficiary</Accordion.Header>
+                <Accordion.Body className="sideMenuTransparent" >
+                <ListGroup >
+                    <ListGroup.Item ><a href="#">Beneficiary Create</a></ListGroup.Item>
+                    <ListGroup.Item><a href="#">Beneficiary List</a></ListGroup.Item>
+                    <ListGroup.Item><a href="#">Delete Beneficiary</a></ListGroup.Item>
+                </ListGroup>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>Send Money</Accordion.Header>
+                <Accordion.Body className="sideMenuTransparent">
+                <ListGroup>
+                    <ListGroup.Item><a href="#">Send Money</a></ListGroup.Item>
+                </ListGroup>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3">
+                <Accordion.Header>Transaction Money</Accordion.Header>
+                <Accordion.Body className="sideMenuTransparent">
+                <ListGroup>
+                    <ListGroup.Item><a href="#">Transaction Money</a></ListGroup.Item>
+                </ListGroup>
+                </Accordion.Body>
+              </Accordion.Item>
             </Accordion>
             
           </Accordion.Body>
         </Accordion.Item>
-
-        
       </Accordion>
     </div>
   );
