16a363fea37d936be8db338f3693f181dd96772c
gjreact <gjreact@gmail.com>
2022-11-21 13:39:56 +0530

    logo with menu

--- src/layout/BasicLayout.js
+++ src/layout/BasicLayout.js
@@ -1,13 +1,22 @@
+import logo from "../asset/images/logo.png"
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "../asset/css/slite.css";
+import RemitterBalancePage from "../pages/DMT/Remitter/RemitterBalance/RemitterBalancePage";
 
 function BasicLayout() {
   return (
     <>
       <Container fluid className="border border-2 margin-0 containerr">
         <Row className="Roww">
-          <Col className="d-none d-md-block bg-dark col-2">dfsd</Col>
+          <Col className="d-none d-md-block bg-dark col-2">
+            <div className="mt-3 d-flex justify-content-center">
+             <img className="align-center" style={{width:"100%",maxWidth:"150px"}} src={logo} alt="Paymento_Logo" />
+            </div>
+            <div>
+              <RemitterBalancePage />
+            </div>
+          </Col>
           <Col className="xs-12 md-10 bg-white">
             
           </Col>
---
+++ src/pages/DMT/Remitter/RemitterBalance/RemitterBalancePage.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+
+function RemitterBalancePage() {
+  return (
+    <div className="mt-5">
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>DMT</Accordion.Header>
+          <Accordion.Body>
+            
+          <Accordion defaultActiveKey="1">
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Remitter</Accordion.Header>
+              <Accordion.Body>
+                <a href="https://www.google.com">ammacheee</a>
+              </Accordion.Body>
+            </Accordion.Item>
+            </Accordion>
+
+            <Accordion defaultActiveKey="1">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Remitter</Accordion.Header>
+              <Accordion.Body>
+                <a href="https://www.google.com">ammacheee</a>
+              </Accordion.Body>
+            </Accordion.Item>
+            </Accordion>
+
+            <Accordion defaultActiveKey="1">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Remitter</Accordion.Header>
+              <Accordion.Body>
+                <a href="https://www.google.com">ammacheee</a>
+              </Accordion.Body>
+            </Accordion.Item>
+            </Accordion>
+
+            <Accordion defaultActiveKey="1">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Remitter</Accordion.Header>
+              <Accordion.Body>
+                <a href="https://www.google.com">ammacheee</a>
+              </Accordion.Body>
+            </Accordion.Item>
+            </Accordion>
+            
+          </Accordion.Body>
+        </Accordion.Item>
+
+        
+      </Accordion>
+    </div>
+  );
+}
+
+export default RemitterBalancePage;
